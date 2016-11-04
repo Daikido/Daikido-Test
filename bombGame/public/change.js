ded79ad3671305ce1777436b0efd7bc05c2ae015
Mike19981008 <Mike40103@gmail.com>
2016-11-04 10:55:39 +0800

    btn

--- bombGame/public/change.js
+++ bombGame/public/change.js
@@ -3,3 +3,12 @@ var ok = document.getElementById("ok");
 function update(data){
     ok.innerText = data;
 }
+
+var btn = document.getElementById("btn");
+var btm=document.getElementById("btm");
+btn.onclick = function(){
+    click(1);
+}
+btm.onclick = function(){
+    click(-1);
+}
