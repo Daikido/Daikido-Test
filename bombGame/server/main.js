c93531ff277197a573b8e1b8d037f2e7b6035490
chiuhans111 <chiuhans111@gmail.com>
2016-11-04 10:20:58 +0800

    global time

--- bombGame/server/main.js
+++ bombGame/server/main.js
@@ -1,10 +1,15 @@
 var mike = require('./mike.js');
-
+var time = 0;
 module.exports = function(io){
     
     // timer handle
     function tick(){
+        
+        mike.update(time);
+        
         io.emit('update', mike.getNumber());
+        
+        time+=14;
     }
 
     setInterval(tick, 14);
