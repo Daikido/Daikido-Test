1bfa115dbb4f56eab1f7243df25c0ce5fd9c9998
Mike19981008 <Mike@MikedeMacBook-Pro.local>
2016-11-13 21:12:22 +0800

    set

--- bombGame/server/mike.js
+++ bombGame/server/mike.js
@@ -11,18 +11,65 @@ var set = function (x, y, data) {
     arr1[x][y] = data;
 }
 var get = function (x, y) {
+    if(x<0||y<0) return 0;
     return arr1[x][y];
 }
-var number=0;
-var time=0;
-exports.update=function(t){
-    var delta = t-time;
+for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; j++) {
+        set(i, j, 0);
+    }
+}
+for (var i = 0; i < 70; i++) {
+    var bombx = Math.floor(Math.random() * 10);
+    var bomby = Math.floor(Math.random() * 10);
+    set(bombx, bomby, 1);
+}
+var test = function (x, y) {
+    var a = 0;
+    for (var i = -1; i < 2; i++) {
+        for (var j = -1; j < 2; j++) {
+            if (get(x + i, y + j))
+                a += 1;
+        }
+    }
+
+    return a;
+}
+function loseGame(){
+    confirm("You are lose !!!");
+}
+var clickBomb=function(x,Y){
+    if(get(x,y))
+        return loseGame;
+    else
+        set(x,y,test(x,y))  
+}
+var setFlag=function(x,y){
+    set(x,y,2);
+}
+function pad(s, l){
+    s = s.toString();
+    if(s.length<l) return pad(s+' ', l);
+    return s;
+}
+function draw(array){
+    var text = array.map(arr=>{
+        return arr.map(a=>{
+            return pad(a,3);
+        }).join('');
+    }).join('\r\n');
+    console.log(text);
+}
+var number = 0;
+var time = 0;
+exports.update = function (t) {
+    var delta = t - time;
     time = t;
 }
-exports.click = function(n){
-    number+=n;
+exports.click = function (n) {
+    number += n;
 }
 
-exports.getNumber = function(){
+exports.getNumber = function () {
     return number;
 }
