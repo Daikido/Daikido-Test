1337b68f7a7120a2c637315c598120aed8a003c4
chiuhans111 <chiuhans111@gmail.com>
2016-11-16 20:43:47 +0800

    chat basic interface done

---
+++ bombGame/public/chat/chat.js
@@ -0,0 +1,16 @@
+// by Hans
+
+function Chat(elementId){
+
+    function Room(name){
+        this.name = name;
+        
+        
+
+    }
+
+    var element = document.getElementById(elementId);
+    this.NewRoom = function(name){
+        
+    }
+}
