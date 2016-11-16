c4494899034dc67ff135ba36ff55e7ae9755099a
chiuhans111 <chiuhans111@gmail.com>
2016-11-16 21:01:06 +0800

    refine chat box

--- bombGame/public/chat/chat.js
+++ bombGame/public/chat/chat.js
@@ -4,10 +4,16 @@ function Chat(elementId){
 
     function Room(name){
         this.name = name;
-        
-        
-
     }
+
+    function getLineElement(title, content, self){
+        var div = document.createElement('div');
+        div.classList.add('block');
+        div.classList.add(self?"right":"left");
+        div.innerHTML = `<span class="title">${title}</span><span class="content">${content}</span>`;
+        return div;
+    }
+
 
     var element = document.getElementById(elementId);
     this.NewRoom = function(name){
