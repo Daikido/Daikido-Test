fad67b1ab95294fd0c2bc340bf1e5d9c0a530680
chiuhans111 <chiuhans111@gmail.com>
2016-11-16 18:38:04 +0800

    clean

--- bombGame/public/main.js
+++ bombGame/public/main.js
@@ -1,35 +1,29 @@
 var socket = io();
 
-function EventRocket(){
+function EventRocket() {
     var eventHandlers = {};
-    this.on = function (event, callback){
-        if(eventHandlers[event]==undefined) eventHandlers[event] = [];
+    this.on = function (event, callback) {
+        if (eventHandlers[event] == undefined) eventHandlers[event] = [];
         eventHandlers[event].push(callback);
     }
 
-    this.go = function (event, data){
-        if(eventHandlers[event]!=undefined)
-        eventHandlers[event].map(data);
+    this.go = function (event, data) {
+        if (eventHandlers[event] != undefined)
+            eventHandlers[event].map(data);
     }
 }
 var just = new EventRocket();
 var hans = new EventRocket();
 
-hans.on("join", function(data){
-    socket.emit('join', {name: data});
-});
+hans.on("join", function (data) { socket.emit('join', { name: data }); });
 
-hans.on("click", function(){
-    socket.emit('click');
-});
+hans.on("click", function () { socket.emit('click'); });
 
-hans.on("say", function(data){
-    socket.emit('message', data);
-});
+hans.on("say", function (data) { socket.emit('message', data); });
 
-socket.on('message', function(data){
-    if(data.command=="leave") just.go("leave", data.name);
-    else just.go("message" , {name: data.name, message: data.message} );
+socket.on('message', function (data) {
+    if (data.command == "leave") just.go("leave", data.name);
+    else just.go("message", { name: data.name, message: data.message });
 });
 
 
